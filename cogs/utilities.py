4fc1cbbfc85ff22b22df4b92f98fb81de71ca961
JoelLucaAdams <joelucadams@gmail.com>
2021-05-29 13:31:52 +0100

    first commit

---
+++ cogs/utilities.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from discord import Embed
+import time
+import subprocess
+import requests
+import os
+from datetime import datetime
+
+
+class Utilities(commands.Cog):
+    """
+    General Utilities
+    """
+
+    @commands.command()
+    async def ping(self, ctx: Context):
+        """
+        Status check
+        """
+        start_time = time.time()
+        message = await ctx.send('pong. `DWSPz latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms`')
+        end_time = time.time()
+        await message.edit(content='pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` ' +
+                                   '`Response time: ' + str(int((end_time - start_time) * 1000)) + 'ms`')
+
+    @commands.command()
+    async def source(self, ctx: Context):
+        """
+        Print a link to the source code
+        """
+        await ctx.send(content='Created by `Joel Adams`\n'
+                               'https://github.com/JoelLucaAdams/RedditBot')
