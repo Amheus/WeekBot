89b4f97a401b6cf3e6aca35ba2e0d87590ee2e8b
Sam <sam333lewis@gmail.com>
2021-08-18 12:46:04 +0100

    generalised stuff in the `helpers.py` file

--- helpers.py
+++ helpers.py
@@ -2,6 +2,18 @@ import configparser
 import sys
 
 
+def format_error_message__value_error(section, option):
+    return f'config.ini: {section}.{option} is required!'
+
+
+def format_error_message__type_error(section, option, required_type):
+    return f'config.ini: {section}.{option} must be of type <{required_type}>!'
+
+
+def format_error_message__format_error(section, option, required_format):
+    return f'config.ini: {section}.{option} must have a format of <{required_format}>!'
+
+
 def validate_config():
     with open("config.ini") as file:
         config = configparser.RawConfigParser(allow_no_value=True)
@@ -10,19 +22,19 @@ def validate_config():
         sys.tracebacklimit = 0
 
         if not config.has_option('discord', 'token'):
-            raise ValueError('A discord token is required in the `config.ini` file!')
+            raise ValueError(format_error_message__value_error('discord', 'token'))
         if not config.has_option('parameters', 'channel-id'):
-            raise ValueError('A channel id is required in the `config.ini` file!')
+            raise ValueError(format_error_message__value_error('discord', 'channel-id'))
 
         if not config.get('parameters', 'channel-id').isdigit():
-            raise ValueError('The channel-id must be an integer!')
+            raise TypeError(format_error_message__type_error('discord', 'channel-id', 'int'))
 
         if config.has_option('parameters', 'time'):
             if len(config.get('parameters', 'time')) != 5:
-                raise ValueError('The time must be of format `HH:MM`!')
+                raise TypeError(format_error_message__format_error('discord', 'time', 'HH:MM'))
 
         for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
             if not config.has_option('videos', day):
-                raise ValueError(f'A video link for "{day}" is missing from the `config.ini` file!')
+                raise ValueError(format_error_message__value_error('videos', day))
 
     sys.tracebacklimit = None
